683ed2260e3bd20a565b75ba30938dac988e8db8
cristichiu <grajdiancristian@gmail.com>
2022-09-04 23:40:09 +0200

    Schimbari majore la fuctiile post si fetch

--- ChiuDBPakage/index.js
+++ ChiuDBPakage/index.js
@@ -1,97 +1,20 @@
-// const fs = require("fs")
+const axios = require("axios")
 
-// class ChiuDB {
-//     constructor(link) {
-//         if(!link) console.log("nu-i link")
-//         this.link = link
-//     }
-
-//     create() { return require("./function/create") }
-//     find() { return require("./function/simpleFind/find") }
-//     findOne() { return require("./function/simpleFind/findOne") }
-//     findAndUpdate() { return require("./function/simpleFind/findAndUpdate") }
-//     findOneAndUpdate() { return require("./function/simpleFind/findOneAndUpdate") }
-//     findAndDelete() { return require("./function/simpleFind/findAndDelete") }
-//     findOneAndDelete() { return require("./function/simpleFind/findOneAndDelete") }
-
-//     MC_find() { return require("./function/multiConditionFind/MC_find") }
-//     MC_findOne() { return require("./function/multiConditionFind/MC_findOne") }
-//     MC_findAndUpdate() { return require("./function/multiConditionFind/MC_findAndUpdate") }
-//     MC_findOneAndUpdate() { return require("./function/multiConditionFind/MC_findOneAndUpdate") }
-//     MC_findAndDelete() { return require("./function/multiConditionFind/MC_findAndDelete") }
-//     MC_findOneAndDelete() { return require("./function/multiConditionFind/MC_findOneAndDelete") }
-// }
-
-// module.exports = ChiuDB
-
-
-
-
-
-
-
-
-
-
-
-async function idk() {
-    const fetchs = await fetch('http://localhost:5000/find/link', {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({status: "ok"}),
-        cache: 'no-cache'
+async function find() {
+    let result = "Error"
+    await axios.post('http://localhost:5000/find/link', {
+        status: "ok"
+    }).then(({data}) => {
+        result = data
     })
-    const json = await fetchs.json()
-    console.log(json)
+    new Promise((resolve, reject) => {
+        if(result) {
+            resolve(result);
+        } else {
+            reject("Error")
+        }
+    });
+    return result
 }
-idk()
 
-
-
-
-
-
-
-
-
-
-
-// function init() {
-//     let fileContent = fs.readFileSync("./file.txt", "utf-8"); let fileContentArray = []
-//     fileContent = fileContent.split(/\n/g); fileContent.pop()
-//     fileContent.forEach(content => { fileContentArray.push(JSON.parse(content)) })
-//     fileContent = fileContentArray; fileContentArray = []
-//     return fileContent
-// }
-
-// function find(search) {
-//     let result = "error";
-//     const fileContent = init()
-    
-//     if(Object.keys(search).length !=1) return result = "Use function myltyFindCondition"
-//     result = fileContent.filter(content => {
-//         if(Object.keys(search)[0] in content) {
-//             return content[Object.keys(search)[0]] == search[Object.keys(search)[0]]
-//         }
-//     })
-
-//     return result
-// }
-
-// function multiCondition(search) {
-//     let result = "error";
-//     const fileContent = init()
-
-//     const check = []
-//     fileContent.forEach(content => {
-//         let verify = []
-//         Object.keys(search).forEach(key => { verify.push(content[key] == search[key]) })
-//         check.push(verify)
-//     })
-//     check.forEach(c => { if(c.every(element => element == true)) result = fileContent[check.indexOf(c)] })
-//     return result
-// }
-
-// const date = Date.now()
-// console.log(find({ userID: "947832764353380362" }))
-// console.log(Date.now() - date)
+module.exports = find
